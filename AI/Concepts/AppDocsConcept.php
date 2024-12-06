8d42ee13213e0a24d61cf07952b1874231f96ebf
Andrej Kabachnik <kabachello@users.noreply.github.com>
2024-12-06 12:54:47 +0100

    DEV DocsContext added

---
+++ AI/Concepts/AppDocsConcept.php
@@ -0,0 +1,56 @@
+<?php
+namespace axenox\GenAI\AI\Concepts;
+
+use axenox\GenAI\Common\AbstractConcept;
+use axenox\GenAI\Exceptions\ConceptIncompleteError;
+
+class AppDocsConcept extends AbstractConcept
+{
+    private $appAlias = null;
+
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\Interfaces\TemplateRenderers\PlaceholderResolverInterface::resolve()
+     */ 
+    public function resolve(array $placeholders) : array
+    {
+        $phVals = [];
+        $phVals[$this->getPlaceholder()] = $this->buildMarkdownDocs();
+        return $phVals;
+    }
+
+    /**
+     * Alias of the app to get the docs from
+     * 
+     * @uxon-property app_alias
+     * @uxon-type metamodel:app
+     * 
+     * @param string $alias
+     * @return \axenox\GenAI\AI\Concepts\AppDocsConcept
+     */
+    protected function setAppAlias(string $alias): AppDocsConcept
+    {
+        $this->appAlias = $alias;
+        return $this;
+    }
+
+    protected function getAppAlias() : string
+    {
+        return $this->appAlias;
+    }
+
+    protected function buildMarkdownDocs() : string
+    {
+        $app = $this->getWorkbench()->getApp($this->getAppAlias());
+        $pathToApp = $app->getDirectoryAbsolutePath();
+        $pathToDocs = $pathToApp . DIRECTORY_SEPARATOR . 'Docs';
+        if (! file_exists($pathToDocs)) {
+            throw new ConceptIncompleteError('Docs not found for app "' . $this->getAppAlias() . '"');
+        }
+        $pathToIndex = $pathToDocs . DIRECTORY_SEPARATOR . 'index.md';
+        $content = file_get_contents($pathToIndex);
+        
+        return $content;
+    }
+}
---
+++ Exceptions/ConceptIncompleteError.php
@@ -0,0 +1,9 @@
+<?php
+namespace axenox\GenAI\Exceptions;
+
+use exface\Core\Exceptions\RuntimeException;
+
+class ConceptIncompleteError extends RuntimeException
+{
+    
+}
