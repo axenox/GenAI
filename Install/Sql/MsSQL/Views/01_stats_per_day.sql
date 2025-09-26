6620f8fe9ef5672b91244e6e3f470ad66730168f
Andrej Kabachnik <andrej.kabachnik@accenture.com>
2025-09-26 11:24:24 +0200

    FIX SQL stats views

--- Install/Sql/MsSQL/Views/01_stats_per_day.sql
+++ Install/Sql/MsSQL/Views/01_stats_per_day.sql
@@ -1,10 +1,15 @@
 CREATE OR ALTER VIEW exf_ai_stats_per_day AS
 SELECT
+    MAX(c.oid) AS oid,
+    MAX(c.created_by_user_oid) AS created_by_user_oid,
+    MAX(c.created_on) AS created_on,
+    MAX(c.modified_by_user_oid) AS modified_on_user_oid,
+    MAX(c.modified_on) AS modified_on,
     CONVERT(DATE, c.created_on) AS [date],
     a.oid AS ai_agent_oid,
     a.name AS ai_agent_name,
     NULL AS ai_agent_connection_name,
-    COUNT(DISTINCT c.oid) AS count_conversations,
+    COUNT(DISTINCT c.oid) AS count_conversation,
     COUNT(c.rating) AS count_ratings,
     AVG(c.rating) AS avg_rating,
     SUM(m.cost) AS sum_cost,
--- Install/Sql/MySQL/Views/01_stats_per_day.sql
+++ Install/Sql/MySQL/Views/01_stats_per_day.sql
@@ -1,10 +1,15 @@
-CREATE OR UPDATE VIEW exf_ai_stats_per_day AS
+CREATE OR REPLACE VIEW exf_ai_stats_per_day AS
 SELECT
+    MAX(c.oid) AS oid,
+    MAX(c.created_by_user_oid) AS created_by_user_oid,
+    MAX(c.created_on) AS created_on,
+    MAX(c.modified_by_user_oid) AS modified_on_user_oid,
+    MAX(c.modified_on) AS modified_on,
     DATE(c.created_on) AS date,
     a.oid AS ai_agent_oid,
     a.name AS ai_agent_name,
     null as ai_agent_connection_name,
-    COUNT(c.oid) AS count_conversations,
+    COUNT(c.oid) AS count_conversation,
     COUNT(c.rating) AS count_ratings,
     AVG(c.rating) AS avg_rating,
     (SELECT SUM(m.cost) FROM exf_ai_message m WHERE m.ai_conversation_oid = c.oid) AS sum_cost,
