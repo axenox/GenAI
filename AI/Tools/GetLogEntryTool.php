a9311634a53ba08461398fe42cb0ed822d403d71
Frxnklyn <brooklynfraenzschky@gmail.com>
2025-11-17 18:09:10 +0100

    DEV GetLogEntryTool now use the LogEntryMarkdwonPrinter

--- AI/Tools/GetLogEntryTool.php
+++ AI/Tools/GetLogEntryTool.php
@@ -4,6 +4,7 @@ namespace axenox\GenAI\AI\Tools;
 use axenox\GenAI\Common\AbstractAiTool;
 use exface\Core\CommonLogic\Actions\ServiceParameter;
 use exface\Core\DataTypes\ComparatorDataType;
+use exface\Core\Facades\DocsFacade\MarkdownPrinters\LogEntryMarkdownPrinter;
 use exface\Core\Factories\DataSheetFactory;
 use exface\Core\Interfaces\WorkbenchInterface;
 
@@ -23,35 +24,10 @@ class GetLogEntryTool extends AbstractAiTool
     public function invoke(array $arguments): string
     {
         list($logId) = $arguments;
-
-
-        //Clean up Log Id 
-        if (stripos($logId, 'log-') !== false) {
-            $logId = str_ireplace('log-', '', $logId);
-            $logId = strtoupper($logId);
-        }
-
         
-        $logFileSheet = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'exface.Core.LOG');
-        $logFileCol = $logFileSheet->getColumns()->addFromExpression('PATHNAME_RELATIVE');
-        $logFileSheet->getFilters()->addConditionFromString('CONTENTS', $logId, ComparatorDataType::IS);
-        $logFileSheet->dataRead();
-        
-        $logFile = $logFileCol->getValue(0);
-        
-        $logEntrySheet = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'exface.Core.LOG_ENTRY');
-        $logEntrySheet->getColumns()->addMultiple([
-            'id', 'levelname', 'message', 'filepath', 'context' , 'channel'
-        ]);
-        $logEntrySheet->getFilters()->addConditionFromString('id',$logId, ComparatorDataType::EQUALS);
-        $logEntrySheet->getFilters()->addConditionFromString('logfile', $logFile, ComparatorDataType::EQUALS);
-        $logEntrySheet->dataRead();
-        $row = $logEntrySheet->getRow(0);
-        $detailsPath = $this->getWorkbench()->filemanager()->getPathToLogDetailsFolder(). '/' . $row['filepath'] . '.json';
+        $printer = new LogEntryMarkdownPrinter($this->workbench,$logId);
 
-        $detailsJson = file_get_contents($detailsPath);
-
-        return $this->getLogContent($detailsPath);
+        return $printer->getMarkdown();
     }
 
     /**
@@ -68,49 +44,4 @@ class GetLogEntryTool extends AbstractAiTool
         ];
     }
     
-    protected function getLogContent(string $filePath) : string{
-
-        $completeLog = file_get_contents($filePath);
-        $jsonLog = json_decode($completeLog, true);
-
-
-        $sections = [
-            'error_tab' => [
-                'title' => '## Error',
-                'collector' => '',
-                'filter' => fn(string $v) => !str_contains(strtolower($v), 'support'),
-            ],
-            'widget_uxon_tab' => [
-                'title' => '## WidgetInformation',
-                'collector' => '',
-                'filter' => fn() => true, // no Filter
-            ],
-        ];
-
-
-        foreach ($jsonLog['widgets'] as $widget) {
-            $id = $widget['id'] ?? null;
-            if (!$id || !isset($sections[$id]) || empty($widget['widgets'])) {
-                continue;
-            }
-
-            foreach ($widget['widgets'] as $innerWidget) {
-                $value = $innerWidget['value'] ?? '';
-                if (($sections[$id]['filter'])($value)) {
-                    $sections[$id]['collector'] .= $value . PHP_EOL;
-                }
-            }
-        }
-
-
-        $output = '';
-        foreach ($sections as $section) {
-            if (!empty($section['collector'])) {
-                $output .= $section['title'] . PHP_EOL . $section['collector'] . PHP_EOL;
-            }
-        }
-        
-        
-        return $output;
-    }
 }
