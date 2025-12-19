40c82a1ce545577295572d877d7c444c4fd4792e
Andrej Kabachnik <andrej.kabachnik@accenture.com>
2025-12-19 12:48:30 +0100

    DEV improved error handling, added AiPromptError exception

---
+++ Exceptions/AiPromptError.php
@@ -0,0 +1,54 @@
+<?php
+namespace axenox\GenAI\Exceptions;
+
+use axenox\GenAI\Interfaces\AiAgentInterface;
+use axenox\GenAI\Interfaces\AiPromptInterface;
+use exface\Core\Exceptions\ExceptionTrait;
+use exface\Core\Exceptions\RuntimeException;
+use exface\Core\Facades\DocsFacade;
+use exface\Core\Widgets\DebugMessage;
+
+class AiPromptError extends RuntimeException
+{
+    private AiAgentInterface $agent;
+    private AiPromptInterface $prompt;
+    
+    public function __construct(AiAgentInterface $agent, AiPromptInterface $prompt, string $message, string $alias, \Throwable $previous = null)
+    {
+        parent::__construct($message, $alias, $previous);
+        $this->agent = $agent;
+        $this->prompt = $prompt;
+    }
+    
+    public function getAgent(): AiAgentInterface
+    {
+        return $this->agent;
+    }
+    
+    public function getPrompt(): AiPromptInterface
+    {
+        return $this->prompt;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see ExceptionTrait::createDebugWidget()
+     */
+    public function createDebugWidget(DebugMessage $debug_widget)
+    {
+        $debug_widget = parent::createDebugWidget($debug_widget);
+        // TODO add useful tabs for the AI agent
+        return $debug_widget;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see ExceptionTrait::createDebugWidget()
+     */
+    public function getLinks(): array
+    {
+        $links = parent::getLinks();
+        $links['AI agent prototype "' . $this->agent->getAlias() . '"'] = DocsFacade::buildUrlToDocsForUxonPrototype($this->agent);
+        return $links;
+    }
+}
