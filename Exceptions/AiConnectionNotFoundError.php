895c637ed14166ac5283f68e6ff30bc4c463b582
Frxnklyn <brooklynfraenzschky@gmail.com>
2026-03-10 14:07:13 +0100

    DEV AiFactory::createAgent use new error AiConnectionNotFoundError (submitted later)

---
+++ Exceptions/AiConnectionNotFoundError.php
@@ -0,0 +1,11 @@
+<?php
+namespace axenox\GenAI\Exceptions;
+
+use axenox\GenAI\Interfaces\AiAgentInterface;
+use exface\Core\Exceptions\NotFoundError;
+use exface\Core\Widgets\DebugMessage;
+
+class AiConnectionNotFoundError extends NotFoundError
+{
+    
+}
