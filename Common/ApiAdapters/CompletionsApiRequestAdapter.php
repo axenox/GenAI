9aad534f392da044ce1a1099622df463104ddb5f
Brooklyn Fränzschky <122269327+Frxnklyn@users.noreply.github.com>
2026-05-06 20:39:56 +0200

    NEW ImportTool to import Datasheets (#47)
    * NEW ImportTool for the AI to import a DataSheet
    
    * ADD ImportTool possibility to use more then one Datasheetshema
    
    * FIX ImportTool Change Uxonvariablename to data_schemas
    
    * ADD ImportAgent::setDataSchemas to give the ai the option to import more datasheets

--- Common/ApiAdapters/CompletionsApiRequestAdapter.php
+++ Common/ApiAdapters/CompletionsApiRequestAdapter.php
@@ -6,6 +6,7 @@ use axenox\GenAI\Interfaces\AiConnectorInterface;
 use axenox\GenAI\Interfaces\AiToolInterface;
 use axenox\GenAI\Interfaces\HttpRequestAdapterInterface;
 use exface\Core\DataTypes\JsonDataType;
+use exface\Core\Interfaces\Actions\ServiceParameterInterface;
 use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 
@@ -65,8 +66,7 @@ class CompletionsApiRequestAdapter implements HttpRequestAdapterInterface
             $requiredArgNames = [];
             foreach($tool->getArguments() as $argument)
             {
-                $argSchema = JsonDataType::convertDataTypeToJsonSchemaType($argument->getDataType());
-                $argSchema['description'] = $argument->getDescription();
+                $argSchema = $this->buildArgumentSchema($argument);
                 $arguments[$argument->getName()] = $argSchema;
                 $requiredArgNames[] = $argument->getName();
             }
@@ -90,6 +90,31 @@ class CompletionsApiRequestAdapter implements HttpRequestAdapterInterface
             );
         }
         return $tools;
+    }
+
+    protected function buildArgumentSchema(ServiceParameterInterface $argument): array
+    {
+        $schema = null;
+        if (method_exists($argument, 'getCustomProperty')) {
+            $schemaJson = $argument->getCustomProperty('json_schema');
+            if (is_string($schemaJson) && trim($schemaJson) !== '') {
+                $decoded = json_decode($schemaJson, true);
+                if (is_array($decoded) && ! empty($decoded)) {
+                    $schema = $decoded;
+                }
+            }
+        }
+
+        if ($schema === null) {
+            $schema = JsonDataType::convertDataTypeToJsonSchemaType($argument->getDataType());
+        }
+
+        $description = $argument->getDescription();
+        if ($description !== '' && ! array_key_exists('description', $schema)) {
+            $schema['description'] = $description;
+        }
+
+        return $schema;
     }
     
     protected function buildJsonMessages(OpenAiApiDataQuery $query) : array
