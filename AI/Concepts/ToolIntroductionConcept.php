337cf794bb4890b01a3f406c6d58d8e894218856
Franklyn <brooklynfraenzschky@gmail.com>
2026-06-29 12:20:59 +0200

    ADD ToolIntroductionConcept NEW variables to customize the introduction

--- AI/Concepts/ToolIntroductionConcept.php
+++ AI/Concepts/ToolIntroductionConcept.php
@@ -11,10 +11,14 @@ use exface\Core\Facades\DocsFacade\MarkdownPrinters\UxonPrototypeMarkdownPrinter
  * Renders introductions for all tools of the current agent.
  *
  * Tool descriptions, mandatory tool rules and UXON prototype descriptions are rendered separately.
+ * Each part can be enabled or disabled individually with UXON boolean properties.
  */
 class ToolIntroductionConcept extends AbstractConcept
 {
     private int $headingLevel = 2;
+    private bool $showDescription = true;
+    private bool $showRules = true;
+    private bool $showPrototypeDescription = true;
 
     protected function getOutput(): string
     {
@@ -26,8 +30,18 @@ class ToolIntroductionConcept extends AbstractConcept
             $prototypeDescription = $this->getPrototypeDescription($tool);
             $content = [];
 
-            if ($description === '') {
+            if (!$this->willShowDescription()) {
+                $description = '';
+            } elseif ($description === '') {
                 $description = $prototypeDescription;
+                $prototypeDescription = '';
+            }
+
+            if (!$this->willShowRules()) {
+                $rules = '';
+            }
+
+            if (!$this->willShowPrototypeDescription()) {
                 $prototypeDescription = '';
             }
 
@@ -115,9 +129,75 @@ class ToolIntroductionConcept extends AbstractConcept
         return $this;
     }
 
+    /**
+     * Set to FALSE to hide the tool description text.
+     *
+     * When the tool has no description, the prototype description is used as a fallback
+     * as long as this option stays enabled.
+     *
+     * @uxon-property show_description
+     * @uxon-type boolean
+     * @uxon-default true
+     *
+     * @param bool $show
+     * @return ToolIntroductionConcept
+     */
+    protected function setShowDescription(bool $show): ToolIntroductionConcept
+    {
+        $this->showDescription = $show;
+        return $this;
+    }
+
+    /**
+     * Set to FALSE to hide the mandatory rules section for each tool.
+     *
+     * @uxon-property show_rules
+     * @uxon-type boolean
+     * @uxon-default true
+     *
+     * @param bool $show
+     * @return ToolIntroductionConcept
+     */
+    protected function setShowRules(bool $show): ToolIntroductionConcept
+    {
+        $this->showRules = $show;
+        return $this;
+    }
+
+    /**
+     * Set to FALSE to hide the UXON prototype description of each tool.
+     *
+     * @uxon-property show_prototype_description
+     * @uxon-type boolean
+     * @uxon-default true
+     *
+     * @param bool $show
+     * @return ToolIntroductionConcept
+     */
+    protected function setShowPrototypeDescription(bool $show): ToolIntroductionConcept
+    {
+        $this->showPrototypeDescription = $show;
+        return $this;
+    }
+
     protected function getHeadingLevel(): int
     {
         return $this->headingLevel;
+    }
+
+    protected function willShowDescription(): bool
+    {
+        return $this->showDescription;
+    }
+
+    protected function willShowRules(): bool
+    {
+        return $this->showRules;
+    }
+
+    protected function willShowPrototypeDescription(): bool
+    {
+        return $this->showPrototypeDescription;
     }
 
     /**
