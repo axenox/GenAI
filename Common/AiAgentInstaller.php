8cb73dbc83191d0c4ae8c84f3e42151847c95897
Franklyn <brooklynfraenzschky@gmail.com>
2026-09-02 11:49:06 +0200

    FIX Naming in Installer from Skill

--- Common/AiAgentInstaller.php
+++ Common/AiAgentInstaller.php
@@ -23,7 +23,7 @@ class AiAgentInstaller extends DataInstaller
     {
         parent::__construct($selectorToInstall, MetaModelInstaller::FOLDER_NAME_MODEL . DIRECTORY_SEPARATOR . 'AI');
         
-        $this->addDataToReplace('axenox.GenAI.AI_SKILL', 'CREATED_ON', 'APP', [], 'Skil_[#ALIAS#].json');
+        $this->addDataToReplace('axenox.GenAI.AI_SKILL', 'CREATED_ON', 'APP', [], 'Skill_[#ALIAS#].json');
         $this->addDataToReplace('axenox.GenAI.AI_AGENT', 'CREATED_ON', 'APP', [], '[#ALIAS#]/01_AI_AGENT.json');
         $this->addDataToReplace('axenox.GenAI.AI_AGENT_VERSION', 'CREATED_ON', 'AI_AGENT__APP', [], '[#AI_AGENT__ALIAS#]/02_AI_AGENT_VERSION.json');
         $this->addDataToReplace('axenox.GenAI.AI_TEST_CASE', 'CREATED_ON', 'APP', [], '[#AI_AGENT__ALIAS#]/03_AI_TEST_CASE.json');
